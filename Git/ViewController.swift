b25bc764a5f4b8f4e84316fcb1baf7ab421bf4e3
Nishit-git <nishit.bhuchar100@gmail.com>
2017-10-06 02:26:27 -0400

    Initial Commit

---
+++ Git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git
+//
+//  Created by Nishit on 2017-10-06.
+//  Copyright © 2017 Nishit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
